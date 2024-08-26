5c50559aa4dac02aec1935c7a5ffae6de163dd5e
mac <magicaltitan7@gmail.com>
2024-08-26 20:15:41 -0300

    updated documentation

--- src/com/taskmanager/util/IllegalCurrentStateException.java
+++ src/com/taskmanager/util/IllegalCurrentStateException.java
@@ -1,5 +1,32 @@
 package com.taskmanager.util;
 
+import com.taskmanager.core.CurrentState;
+
+/**
+ * Throws when an application attempts to change {@code CurrentState} to a
+ * current state value lower than or equal to the present value. For example:
+ * <ul>
+ *     <li>{@code CurrentState.IN_PROGRESS} is set to {@code
+ *     CurrentState.NOT_FINISHED}</li>
+ *     <li>{@code CurrentState.FINISHED} is set to {@code
+ *     CurrentState.IN_PROGRESS}</li>
+ *     <li>{@code CurrentState.FINISHED} is set to {@code
+ *     CurrentState.NOT_STARTED}</li>
+ *     <li>{@code CurrentState.IN_PROGRESS} is set to {@code
+ *     CurrentState.IN_PROGRESS}</li>
+ *     <li>{@code CurrentState.FINISHED} is set to {@code
+ *     CurrentState.IN_PROGRESS}</li>
+ *     <li>{@code CurrentState.NOT_STARTED} is set to {@code
+ *     CurrentState.NOT_STARTED}</li>
+ * </ul>
+ *
+ * Applications should throw instances of this class to indicate other
+ * illegal changing of {@code CurrentState}
+ *
+ * @author mac
+ * @see     CurrentState
+ * @since 1.0
+ */
 public class IllegalCurrentStateException extends Exception{
     public IllegalCurrentStateException(String message) {
         super(message);
